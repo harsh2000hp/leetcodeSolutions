2312ba42a458018e79a772643de6bf83ad6359c3
harsh2000hp <67678467+harsh2000hp@users.noreply.github.com>
2022-09-30 00:55:06 -0700

    Time: 75 ms (63.56%), Space: 14.5 MB (34.78%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -2,14 +2,16 @@ class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         s_table = {}
         t_table = {}
-        for char in s:
-            if char in s_table:
-                s_table[char] += 1
+        if len(s) != len(t):
+            return False
+        for char in range(len(s)):
+            if s[char] in s_table:
+                s_table[s[char]] += 1
             else:
-                s_table[char] = 1
-        for char in t:
-            if char in t_table:
-                t_table[char] += 1
+                s_table[s[char]] = 1
+            if t[char] in t_table:
+                t_table[t[char]] += 1
             else:
-                t_table[char] = 1
+                t_table[t[char]] = 1
+        
         return s_table == t_table
