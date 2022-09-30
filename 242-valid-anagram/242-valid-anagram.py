57306bfd7ab57aed8a6af3031fec22dce6a2c477
harsh2000hp <67678467+harsh2000hp@users.noreply.github.com>
2022-09-30 00:49:43 -0700

    Time: 91 ms (44.08%), Space: 14.5 MB (67.03%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -1,3 +1,15 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return all(s.count(x) == t.count(x) for x in 'abcdefghijklmnopqrstuvwxzy')
+        s_table = {}
+        t_table = {}
+        for char in s:
+            if char in s_table:
+                s_table[char] += 1
+            else:
+                s_table[char] = 1
+        for char in t:
+            if char in t_table:
+                t_table[char] += 1
+            else:
+                t_table[char] = 1
+        return s_table == t_table
