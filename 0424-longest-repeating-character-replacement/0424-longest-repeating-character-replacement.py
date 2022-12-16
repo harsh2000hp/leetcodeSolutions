e4d84b4feca4b740b88fdc6270660c6d2b6dca9a
harsh2000hp <67678467+harsh2000hp@users.noreply.github.com>
2022-12-16 14:00:02 -0800

    Time: 637 ms (11.49%), Space: 13.9 MB (58.26%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -1,12 +1,24 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        count = {}
-        res = 0
-        l = 0
-        for r in range(len(s)):
-            count[s[r]] = 1+count.get(s[r], 0)
-            while (r-l+1) - max(count.values()) > k:
-                count[s[l]] -= 1
-                l += 1
-            res = max(res, r-l+1)
-        return res
+        lo = 1
+        hi = len(s) + 1
+        while lo + 1 < hi:
+            mid = lo + (hi-lo) // 2
+            if self.__can_make_valid_substring(s, mid, k):
+                lo = mid
+            else:
+                hi = mid
+        return lo
+    def __can_make_valid_substring(self, s: str, substring_length: int, k: int):
+        freq_map = {}
+        max_frequency = 0
+        start = 0
+        for end in range(len(s)):
+            freq_map[s[end]] = freq_map.get(s[end], 0) + 1
+            if end+ 1 - start > substring_length:
+                freq_map[s[start]] -= 1
+                start += 1
+            max_frequency = max(max_frequency, freq_map[s[end]])
+            if substring_length - max_frequency <= k:
+                return True
+        return False
