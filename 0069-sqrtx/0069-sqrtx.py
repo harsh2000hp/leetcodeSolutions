88d294ecd8a0be0221f0230d300f437da17a114c
harsh2000hp <67678467+harsh2000hp@users.noreply.github.com>
2023-01-03 16:29:30 -0800

    Time: 34 ms (93.34%), Space: 13.8 MB (54.96%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -1,8 +1,15 @@
-from math import e, log
 class Solution:
-    def mySqrt(self, x):
+    def mySqrt(self, x: int) -> int:
         if x < 2:
             return x
-        left = int(e**(0.5 * log(x)))
-        right = left + 1
-        return left if right * right > x else right
+        left, right = 2, x // 2
+        while left <= right:
+            pivot = left + (right-left) // 2
+            num = pivot * pivot
+            if num > x:
+                right = pivot-1
+            elif num < x:
+                left = pivot + 1
+            else:
+                return pivot
+        return right
